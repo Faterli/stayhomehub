e1d65519965d12b71c3dd5b4f2753086a92cafe3
lihuayan <lihuayan@smzdm.com>
2020-04-29 17:00:31 +0800

    用户注册接口

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -14,7 +14,7 @@ class VerificationCodesController extends Controller
         $phone = $request->phone;
 
         if (!app()->environment('production')) {
-            $code = '987023';
+            $code = '1234';
         } else {
             // 生成4位随机数，左侧补0
             $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,7 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
     // 短信验证码
     Route::post('user/code', 'VerificationCodesController@store')
         ->name('verificationCodes.store');
+    // 用户注册
+    Route::post('user/register', 'UsersController@store')
+        ->name('users.store');
 });
