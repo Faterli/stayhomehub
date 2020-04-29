7438c3df9a40ff9fa7ae13b6ba01e8666334ac5a
lihuayan <lihuayan@smzdm.com>
2020-04-29 17:04:54 +0800

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,23 @@
 
 use Illuminate\Http\Request;
 
-Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
-    // 短信验证码
-    Route::post('user/code', 'VerificationCodesController@store')
-        ->name('verificationCodes.store');
-    // 用户注册
-    Route::post('user/register', 'UsersController@store')
-        ->name('users.store');
-});
+Route::prefix('v1')
+    ->namespace('Api')
+    ->name('api.v1.')
+    ->group(function () {
+
+        Route::middleware('throttle:' . config('api.rate_limits.sign'))
+            ->group(function () {
+                // 短信验证码
+                Route::post('user/code', 'VerificationCodesController@store')
+                    ->name('verificationCodes.store');
+                // 用户注册
+                Route::post('user/register', 'UsersController@store')
+                    ->name('users.store');
+            });
+
+        Route::middleware('throttle:' . config('api.rate_limits.access'))
+            ->group(function () {
+
+            });
+    });
